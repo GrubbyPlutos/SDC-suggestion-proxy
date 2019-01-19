0535e33bfbfcf65140aa946cbb005e296b87b4a8
Jessica <jessicarhodson@gmail.com>
2019-01-19 15:02:40 -0800

    Configure proxy to work with suggestions service

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/:id', express.static(path.join(__dirname, 'public')));
+app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 // app.use(bodyParser.text())
 app.listen(port, () => {
